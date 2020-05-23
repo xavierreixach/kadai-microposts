edbc8a10225bb1565c957530da87f096de6c097a
xavier.reixach <xavierreixac@gmail.com>
2020-05-23 13:59:53 +0000

    fix-favorite

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -13,11 +13,11 @@
                     <div>
                         @if (Auth::user()->is_favorite($micropost->id))
                             {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('Unfollow', ['class' => "btn btn-success btn-sm"]) !!}
+                                {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
                             {!! Form::close() !!}
                         @else
                             {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-                                {!! Form::submit('Follow', ['class' => "btn btn-light btn-sm"]) !!}
+                                {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
                             {!! Form::close() !!}
                         @endif
                     </div>
